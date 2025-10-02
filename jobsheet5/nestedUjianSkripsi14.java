cc536274845d42cab215fe033b63f9bd0e0f7a13
MochDedy <mochdedy96@gmail.com>
2025-10-02 15:56:50 +0700

    Percobaan 3

--- jobsheet5/nestedUjianSkripsi14.java
+++ jobsheet5/nestedUjianSkripsi14.java
@@ -1,5 +1,7 @@
 package jobsheet5;
+
 import java.util.Scanner;
+
 public class nestedUjianSkripsi14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,15 +14,15 @@ public class nestedUjianSkripsi14 {
         int bimbinganP1 = sc.nextInt();
         System.out.print("Masukkan jumlah log bimbingan pembimbing 2: ");
         int bimbinganP2 = sc.nextInt();
-        
+
         if (bebasKompen.equalsIgnoreCase("Ya")) {
             if (bimbinganP1 >= 8 && bimbinganP2 >= 4) {
                 pesan = "semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skirpsi";
-            }else if (bimbinganP1 < 8 && bimbinganP2 < 4 ) {
+            } else if (bimbinganP1 < 8 && bimbinganP2 < 4) {
                 pesan = "Gagal! log bimbingan P1 kurang dari 8 kali dan P2 kurang dari 4 kali";
-            }else if (bimbinganP1 < 8) {
+            } else if (bimbinganP1 < 8) {
                 pesan = "Gagal! log bimbingan P1 belum mencapai 8 kali";
-            }else {
+            } else {
                 pesan = "Gagal! log bimbingan P2 belum mencapai 4 kali";
             }
         } else {
