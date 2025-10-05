860bc3421174dff845a9923b2ffa4cdfba8af32a
MochDedy <mochdedy96@gmail.com>
2025-10-05 16:08:08 +0700

    Tugas 3

--- jobsheet5/wifi14.java
+++ jobsheet5/wifi14.java
@@ -9,22 +9,18 @@ public class wifi14 {
         System.out.print("Masukkan pengguna (dosen/mahasiswa): ");
         String pengguna = input.nextLine();
 
-        switch (pengguna) {
-            case "dosen":
-                System.out.println("Akses wifi diberikan (dosen)");
-                break;
-            case "mahasiswa":
-                System.out.print("Masukkan jumlah SKS: ");
-                int sks = input.nextInt();
-
-                if (sks >= 12) {
-                    System.out.println("Akses WiFi diberikan (mahasiswa aktif)");
-                } else {
-                    System.out.println("Akses ditolak, SKS kurang dari 12");
-                }
-                break;
-            default:
-                System.out.println("Akses ditolak");
+        if (pengguna.equalsIgnoreCase("dosen")) {
+            System.out.println("Akses wifi diberikan (dosen)");
+        } else if (pengguna.equalsIgnoreCase("mahasiswa")) {
+            System.out.print("Masukkan jumlah SKS anda: ");
+            int sks = input.nextInt();
+            if (sks >= 12) {
+                System.out.println("Akses Wifi diberikan (mahasiswa aktif)");
+            } else {
+                System.out.println("Akses ditolak, jumlah SKS kamu kurang dari 12!");
+            }
+        } else {
+            System.out.println("Akses ditolak");
         }
     }
 }
